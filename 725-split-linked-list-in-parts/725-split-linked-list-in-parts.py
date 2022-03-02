b2a66ba5f162db0bafef9a0a8252a179645d473a
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-03-02 20:12:05 +0800

    Time: 55 ms (51.93%), Space: 14.3 MB (86.71%) - LeetHub

---
+++ 725-split-linked-list-in-parts/725-split-linked-list-in-parts.py
@@ -0,0 +1,50 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def splitListToParts(self, head: Optional[ListNode], k: int) -> List[Optional[ListNode]]:
+#         n = 0
+#         p = head
+#         while p != None:
+#             n += 1
+#             p = p.next
+        
+#         quotinent , reminder = n // k , n % k  # 3, 1 
+        
+#         array = [None for i in range(k)]
+        
+#         p = head
+#         i = 0
+#         while i < k and p:
+#             array[i] = p
+#             split = quotinent + ( 1 if i < reminder else 0)
+#             for i in range(split - 1):
+#                 p = p.next
+#             p2 = p.next
+#             p.next = None
+#             p = p2
+#             i+=1
+#         return array
+
+        n = 0
+        node = head
+        while node:
+            n += 1
+            node = node.next
+        quotient, remainder = n // k, n % k
+
+        parts = [None for _ in range(k)]
+        i, curr = 0, head
+        while i < k and curr:
+            parts[i] = curr
+            part_size = quotient + (1 if i < remainder else 0)
+            for _ in range(part_size - 1):
+                curr = curr.next
+            next = curr.next
+            curr.next = None
+            curr = next
+            i += 1
+        return parts
+        
