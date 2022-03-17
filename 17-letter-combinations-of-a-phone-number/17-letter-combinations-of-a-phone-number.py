d878d260eaa527293425ecd53f010b0757726aee
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-03-17 12:34:44 +0800

    Time: 41 ms (60.65%), Space: 14.1 MB (24.02%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.py
@@ -10,17 +10,38 @@ class Solution:
             "8":["t","u","v"],
             "9":["w","x","y","z"],
         }
-        final = []
-        for i in digits:
-            if final == []:
-                final = table[i]
-                continue
-            append_li = []
-            for j in final:
-                for t in table[i]:
-                    append_li.append(j+t)
-            final = append_li
-        return final
+        
+        ######## first ###################
+        # final = []
+        # for i in digits:
+        #     if final == []:
+        #         final = table[i]
+        #         continue
+        #     append_li = []
+        #     for j in final:
+        #         for t in table[i]:
+        #             append_li.append(j+t)
+        #     final = append_li
+        # return final
+        
+        
+        ######## second ###################
+        
+        return_li = []
+        length = len(digits)
+        if length == 0:return[]
+        def backtracking(start_index,path):
+            if len(path) == length:
+                return_li.append("".join(path))
+                return
+            for item in table[digits[start_index]]:
+                path.append(item)
+                backtracking(start_index+1,path)
+                path.pop()
+        backtracking(0,[])
+        return   return_li
+        
+            
                 
 
         
