cf341821d90687d084cb1fe904565d88968699db
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-03-20 18:23:15 +0800

    Time: 231 ms (87.51%), Space: 16.2 MB (50.09%) - LeetHub

---
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        new = []
+        for i in nums:
+            new.append(i*i)
+        new.sort()
+        return new
+        
