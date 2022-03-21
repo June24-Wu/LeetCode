210af1192a6ecf6120150b5aafa70bb142bce2c9
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-03-21 11:50:06 +0800

    Time: 36 ms (88.27%), Space: 14.3 MB (36.86%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -8,21 +8,42 @@ class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if root == None:return root
         final = []
+        sub_final = []
         stack = [root]
+        cnt = 0
+        size = len(stack)
         
+        # --------1
+        # while stack != []:
+        #     sub_final = []
+        #     for i in stack:
+        #         sub_final.append(i.val)
+        #     final.append(sub_final)
+        #     new_stack = stack.copy()
+        #     stack = []
+        #     while new_stack != []:
+        #         a = new_stack.pop(0)
+        #         if a.left != None:
+        #             stack.append(a.left)
+        #         if a.right != None:
+        #             stack.append(a.right)
+        # return final
+        
+        
+        # --------2
         while stack != []:
-            sub_final = []
-            for i in stack:
-                sub_final.append(i.val)
-            final.append(sub_final)
-            new_stack = stack.copy()
-            stack = []
-            while new_stack != []:
-                a = new_stack.pop(0)
-                if a.left != None:
-                    stack.append(a.left)
-                if a.right != None:
-                    stack.append(a.right)
+            a = stack.pop(0)
+            sub_final.append(a.val)
+            if a.left != None:
+                stack.append(a.left)
+            if a.right != None:
+                stack.append(a.right)
+            cnt += 1
+            if cnt == size:
+                final.append(sub_final)
+                sub_final = []
+                size = size = len(stack)
+                cnt = 0
         return final
             
                 
