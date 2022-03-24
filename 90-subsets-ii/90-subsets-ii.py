8398b1998dbaac9b2826d79a75456456b2a39916
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-03-24 18:02:28 +0800

    Time: 76 ms (14.76%), Space: 14.1 MB (94.70%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,18 +1,35 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        return_li = [[]]
         nums.sort()
-        q = [[]]
         length = len(nums)
-        for i in nums:
-            for j in range(len(q)):
-                q.append(q[j] + [i])
+        def backtracking(start_index,path):
+            if start_index == length:
+                return
+            for i in range(start_index,length):
+                if i > start_index and nums[i] == nums[i-1]:
+                    continue
+                path.append(nums[i])
+                return_li.append(path[:])
+                backtracking(i+1,path)
+                path.pop()
+        backtracking(0,[])
+        return return_li
+            
+    
+#         nums.sort()
+#         q = [[]]
+#         length = len(nums)
+#         for i in nums:
+#             for j in range(len(q)):
+#                 q.append(q[j] + [i])
         
-        l2 = []
-        for i in q:
-            i = str(i).replace("[","").replace("]","")
-            l2.append(i)
-        l2 = list(set(l2))
-        l3 = []
-        for i in l2:
-            l3.append(i.split(","))
-        return l3
+#         l2 = []
+#         for i in q:
+#             i = str(i).replace("[","").replace("]","")
+#             l2.append(i)
+#         l2 = list(set(l2))
+#         l3 = []
+#         for i in l2:
+#             l3.append(i.split(","))
+#         return l3
