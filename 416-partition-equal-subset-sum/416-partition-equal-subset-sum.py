6f370602b00356b97c3b385ddd54641b81e75020
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-03-31 15:02:37 +0800

    Time: 4392 ms (13.09%), Space: 14.2 MB (81.84%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.py
@@ -3,7 +3,7 @@ class Solution:
         target = sum(nums)
         if target % 2 == 1:return False
         target = target // 2
-        dp = [0 for _ in range(20000)]
+        dp = [0 for _ in range(target+1)]
         for num in nums:
             for j in range(target,num-1,-1):
                 dp[j] = max(dp[j],dp[j-num] + num)
