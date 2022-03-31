ce018971be3ef8a6194e4ee8c4e66a80d57c29c4
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-03-31 13:30:42 +0800

    Time: 36 ms (83.63%), Space: 13.9 MB (20.57%) - LeetHub

---
+++ 343-integer-break/343-integer-break.py
@@ -0,0 +1,11 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        dp = [0 for _ in range(n+1)]
+        dp[2] = 1
+        
+        for i in range(3,len(dp)):
+            for j in range(0,i-1):
+                dp[i] = max(dp[i],max((i-j)*j,dp[i-j]*j))
+        print(dp)
+        return dp[-1]
+        
