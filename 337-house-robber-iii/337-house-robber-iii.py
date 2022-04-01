847dd8a7659bb13807cb5bb0cb28f7ad19db5e00
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-04-01 16:49:38 +0800

    Time: 95 ms (18.64%), Space: 16.2 MB (40.43%) - LeetHub

--- 337-house-robber-iii/337-house-robber-iii.py
+++ 337-house-robber-iii/337-house-robber-iii.py
@@ -6,19 +6,37 @@
 #         self.right = right
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
-        hashmap = {}
-        def __rob(root: Optional[TreeNode]) -> int:
-            if not root: return 0
-            if root in hashmap.keys():return hashmap[root]
-            robVal = root.val
-            if root.left:
-                robVal += __rob(root.left.left) + __rob(root.left.right)
-            if root.right:
-                robVal += __rob(root.right.left) + __rob(root.right.right)
-            not_rob = __rob(root.left) + __rob(root.right)
-            maxVal = max(robVal,not_rob)
-            hashmap[root] = maxVal
-            return maxVal
-        return __rob(root)
+        a = self.__rob(root)
+        return max(a[1],a[0])
+        
+        
+        
+    def __rob(self,node):
+        if not node:return (0,0)  # steal , not steal
+        
+        val = node.val
+        left = self.__rob(node.left) # (steal,not steal)
+        right = self.__rob(node.right) # (steal,not steal)
+        
+        steal = val + left[1] + right [1]
+        not_steal = max(left[0],left[1]) + max(right[0],right[1])
+        
+        return (steal,not_steal)
+        
+        
+        # hashmap = {}
+        # def __rob(root: Optional[TreeNode]) -> int:
+        #     if not root: return 0
+        #     if root in hashmap.keys():return hashmap[root]
+        #     robVal = root.val
+        #     if root.left:
+        #         robVal += __rob(root.left.left) + __rob(root.left.right)
+        #     if root.right:
+        #         robVal += __rob(root.right.left) + __rob(root.right.right)
+        #     not_rob = __rob(root.left) + __rob(root.right)
+        #     maxVal = max(robVal,not_rob)
+        #     hashmap[root] = maxVal
+        #     return maxVal
+        # return __rob(root)
         
         
