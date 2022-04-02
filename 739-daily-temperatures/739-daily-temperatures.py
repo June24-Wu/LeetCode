d06efd200bd7237c77842ff067fa2d450e077575
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-04-02 15:20:41 +0800

    Time: 1320 ms (78.73%), Space: 25 MB (50.68%) - LeetHub

--- 739-daily-temperatures/739-daily-temperatures.py
+++ 739-daily-temperatures/739-daily-temperatures.py
@@ -1,28 +1,22 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        # p1 = 0
-        # p2 = 0
-        # output = [0 for _ in range(len(temperatures))]
-        # while p1 < len(temperatures):
-        #     p2 = p2 + 1
-        #     if p2 >= len(temperatures):
-        #         p1 += 1
-        #         p2 = p1
-        #     elif temperatures[p2] > temperatures[p1]:
-        #         output[p1] = p2-p1
-        #         p1 += 1
-        #         p2 = p1
-        # return output
-        length = len(temperatures)
-        output = [0 for _ in range(length)]
+        return_li = [0 for _ in range(len(temperatures))]
+        
         stack = []
-        for i in range(length):
-            temperature = temperatures[i]
-            while stack != [] and temperature > temperatures[stack[-1]]:
-                val = stack.pop()
-                output[val] = i - val
-            stack.append(i)
-        return output
+        
+        for i in range(len(temperatures)):
+            if stack == []:
+                stack.append(i)
+            else:
+                while stack != []:
+                    last = stack.pop()
+                    if temperatures[i] > temperatures[last]:
+                        return_li[last] = i - last
+                    else:
+                        stack.append(last)
+                        break
+                stack.append(i)
+        return return_li
             
         
             
