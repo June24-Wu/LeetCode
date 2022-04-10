6350be90d5b4c058d28ce5f6fa075163a356f090
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-04-10 19:30:20 +0800

    Time: 852 ms (53.90%), Space: 25.2 MB (8.60%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.py
+++ 912-sort-an-array/912-sort-an-array.py
@@ -1,28 +1,66 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def sort(left,right):
-            mid = random.randint(left,right)
-            mid_num = nums[mid]
-            nums[mid] , nums[right] = nums[right] , nums[mid]
-            
-            p = left
-            
-            for i in range(left,right):
-                if nums[i] < mid_num:
-                    nums[i] , nums[p] = nums[p] , nums[i]
-                    p += 1
-            
-            nums[p] , nums[right] =  nums[right] , nums[p]
-            return p
         
-        def sort2(l,r):
-            if l >= r:return
-            mid = sort(l,r)
-            sort2(mid+1,r)
-            sort2(l,mid-1)
         
-        sort2(0,len(nums)-1)
-        
+        # Merge Sort
+        def sort(l,r):
+            if l >= r: return
+            mid = (l+r) // 2
+            sort(l,mid)
+            sort(mid+1,r)
+            
+            left_arr = nums[l:mid+1]
+            right_arr = nums[mid+1:r+1]
+            arr = []
+            
+            p1 = 0; p2 = 0
+            
+            while p1 < len(left_arr) and p2 < len(right_arr):
+                if left_arr[p1] <= right_arr[p2]:
+                    arr.append(left_arr[p1])
+                    p1 += 1
+                else:
+                    arr.append(right_arr[p2])
+                    p2 += 1
+            if p1 >= len(left_arr):
+                for i in range(p2,len(right_arr)):
+                    arr.append(right_arr[i])
+            else:
+                for i in range(p1,len(left_arr)):
+                    arr.append(left_arr[i])
+            nums[l:r+1] = arr[:]
+            return 
+        sort(0,len(nums)-1)
         return nums
+            
+            
+        
+        
+        # Quick Sort
+        
+#         def sort(left,right):
+#             mid = random.randint(left,right)
+#             mid_num = nums[mid]
+#             nums[mid] , nums[right] = nums[right] , nums[mid]
+            
+#             p = left
+            
+#             for i in range(left,right):
+#                 if nums[i] < mid_num:
+#                     nums[i] , nums[p] = nums[p] , nums[i]
+#                     p += 1
+            
+#             nums[p] , nums[right] =  nums[right] , nums[p]
+#             return p
+        
+#         def sort2(l,r):
+#             if l >= r:return
+#             mid = sort(l,r)
+#             sort2(mid+1,r)
+#             sort2(l,mid-1)
+        
+#         sort2(0,len(nums)-1)
+        
+#         return nums
             
         
