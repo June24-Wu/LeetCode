afbc2be650ee6cd743cdf903e6a3c87a32deade2
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-04-12 10:22:30 +0800

    Time: 152 ms (89.86%), Space: 19 MB (10.85%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,21 +1,37 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        
         intervals.sort(key = lambda x: (x[0],x[1]))
-        merge = []
-        if len(intervals) <= 1: return intervals
-        
-        left = intervals[0][0]
-        right = intervals[0][1]
-        
+
+        left = intervals[0][0] ; right = intervals[0][1]
+        rt = []
         for i in range(1,len(intervals)):
-            if intervals[i][0] <= right:
+            if intervals[i][0]  <= right:
                 right = max(right,intervals[i][1])
             else:
-                merge.append([left,right])
+                rt.append([left,right])
                 left = intervals[i][0]
                 right = intervals[i][1]
-        merge.append([left,right])
-        return merge
+        rt.append([left,right])
+        return rt
+                
+            
+#         intervals.sort(key = lambda x: (x[0],x[1]))
+#         merge = []
+#         if len(intervals) <= 1: return intervals
+        
+#         left = intervals[0][0]
+#         right = intervals[0][1]
+        
+#         for i in range(1,len(intervals)):
+#             if intervals[i][0] <= right:
+#                 right = max(right,intervals[i][1])
+#             else:
+#                 merge.append([left,right])
+#                 left = intervals[i][0]
+#                 right = intervals[i][1]
+#         merge.append([left,right])
+#         return merge
             
         
         
