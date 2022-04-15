b6070e7f8464ac6007b958f3954dcf39fb104dff
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-04-15 10:43:15 +0800

    Time: 96 ms (81.23%), Space: 21.2 MB (94.68%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -1,25 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def __init__(self):
-        self.maxSum = float("-inf")
-
-    def maxPathSum(self, root: TreeNode) -> int:
-        def maxGain(node):
-            if not node:
-                return 0
-
-            # 递归计算左右子节点的最大贡献值
-            # 只有在最大贡献值大于 0 时，才会选取对应子节点
-            leftGain = max(maxGain(node.left), 0)
-            rightGain = max(maxGain(node.right), 0)
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        rtVal = - float("inf")
+        def dfs(node):
+            nonlocal rtVal
+            if node == None: return 0
+            leftVal = max(0,dfs(node.left)) ; rightVal = max(0,dfs(node.right))
+            val = node.val + leftVal + rightVal
+            rtVal = max(val,rtVal)
+            return node.val + max(leftVal,rightVal)
             
-            # 节点的最大路径和取决于该节点的值与该节点的左右子节点的最大贡献值
-            priceNewpath = node.val + leftGain + rightGain
-            
-            # 更新答案
-            self.maxSum = max(self.maxSum, priceNewpath)
+            # return node.val + leftVal + rightVal
         
-            # 返回节点的最大贡献值
-            return node.val + max(leftGain, rightGain)
-   
-        maxGain(root)
-        return self.maxSum
+        dfs(root)
+        return rtVal
+        
