f15ba9c1de3411ca84901027143d48f852d937f8
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-04-19 17:16:48 +0800

    Time: 61 ms (52.55%), Space: 16.6 MB (46.74%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -7,20 +7,13 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         
-        def dfs(node,min_val = -float("inf"),max_val = float("inf")):
-            if node == None:
-                return True
-            if node.val >= max_val or node.val <= min_val:
+        
+        def dfs(node,left,right):
+            if node == None:return True
+            elif node.val <= left or node.val >= right:
                 return False
-            return dfs(node.left,min_val,node.val) and dfs(node.right,node.val,max_val)
-        return dfs(root)
-            # if node.left == None and node.right == None:
-            #     return True
-            # elif node.left == None:
-            #     return node.val < node.right.val and dfs(node.right)
-            # elif node.right == None:
-            #     return node.val > node.left.val and dfs(node.left)
-            # return node.val < node.right.val and dfs(node.right) and node.val > node.left.val and dfs(node.left)
-        # return dfs(root)
+            else:
+                return dfs(node.left,left,node.val) and dfs(node.right,node.val,right)
+        return dfs(root,-float("inf"),float("inf"))
             
         
