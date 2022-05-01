ace01fd07d3f7c872289496f499da9c35d360fed
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-05-01 19:27:07 +0800

    Time: 55 ms (49.21%), Space: 13.9 MB (86.49%) - LeetHub

---
+++ 486-predict-the-winner/486-predict-the-winner.py
@@ -0,0 +1,15 @@
+class Solution:
+    def PredictTheWinner(self, nums: List[int]) -> bool:
+        
+        # dp 
+        length = len(nums)
+        dp = [0 for _ in range(length)]
+        for i in range(length-1,-1,-1):
+            temp = dp.copy()
+            dp[i] = nums[i]
+            for j in range(i+1,length):
+                dp[j] = max(nums[j]-dp[j-1],nums[i] - temp[j])
+            # print(dp)
+        return dp[-1] >= 0 
+                
+        
