485a97b39a00c89cfb525b70f64c340d877ada21
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-05-05 19:45:17 +0800

    Time: 1076 ms (26.98%), Space: 15.8 MB (79.22%) - LeetHub

--- 480-sliding-window-median/480-sliding-window-median.py
+++ 480-sliding-window-median/480-sliding-window-median.py
@@ -1,16 +1,55 @@
+class Median:
+    def __init__(self):
+        self.min = []
+        self.max = []
+    def add(self,val:float):
+        if self.min == []:
+            heapq.heappush(self.min,-val)
+            return
+        if self.max == []:
+            heapq.heappush(self.max,val)
+            return
+        if val > - self.min[0]:
+            heapq.heappush(self.max,val)
+        else:
+            heapq.heappush(self.min,-val)
+        self.restruct()
+        return
+    def restruct(self):
+        while abs(len(self.min) - len(self.max)) >= 1:
+            if len(self.min) - len(self.max) == 1:
+                break
+            if len(self.min) - len(self.max) > 1:
+                val = - heapq.heappop(self.min)
+                heapq.heappush(self.max,val)
+            else:
+                val = - heapq.heappop(self.max)
+                heapq.heappush(self.min,val)
+        return
+    def get(self):
+        if len(self.min) == len(self.max):
+            return (self.max[0] - self.min[0]) / 2
+        return  - self.min[0]
+    def delete(self,val):
+        if val <=  - self.min[0]:
+            self.min.pop(self.min.index(-val))
+            heapq.heapify(self.min)
+        else:
+            self.max.pop(self.max.index(val))
+            heapq.heapify(self.max)
+        self.restruct()
 class Solution:
     def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
-        def median(array):
-            array.sort()
-            if len(array) % 2 == 1:
-                return array[len(array)//2]
-            else:
-                return (array[len(array)//2] + array[len(array)//2 - 1]) / 2
-        
-        
-        rt = []
-        for i in range(k,len(nums)+1):
-            subArray = nums[i-k:i]
-            rt.append(median(subArray))
-        return rt
+        median = Median()
+        for i in range(k):
+            median.add(nums[i])
+        ans = [median.get()]
+        for i in range(k,len(nums)):
+            # print(median.min)
+            # print(median.max)
+            median.delete(nums[i-k])
+            median.add(nums[i])
+            ans.append(median.get())
+        return ans
+            
         
