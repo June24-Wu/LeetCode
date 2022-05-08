a3538c9683e830980ff7ab5b94d9164207096a40
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-05-08 14:14:07 +0800

    Time: 237 ms (39.91%), Space: 15.9 MB (36.39%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,12 +1,30 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        length = len(nums) / 2
-        table = {}
-        for i in nums:
-            if i not in table:
-                table[i] = 0
-            table[i] += 1
-        for i in table:
-            if table[i] >= length:
-                return i
+        # length = len(nums) / 2
+        # table = {}
+        # for i in nums:
+        #     if i not in table:
+        #         table[i] = 0
+        #     table[i] += 1
+        # for i in table:
+        #     if table[i] >= length:
+        #         return i
+        
+        
+        # 分治
+        def divide(left,right):
+            if left == right:
+                return nums[left]
+            mid = (left+right) // 2
+            left_majority = divide(left,mid)
+            right_majority = divide(mid+1,right)
+            if left_majority == right_majority:
+                return left_majority
+            left_majority_cnt = sum(1 for i in nums[left:mid+1] if i == left_majority)
+            right_majority_cnt = sum(1 for i in nums[mid+1:right+1] if i == right_majority)
+            if left_majority_cnt > right_majority_cnt:
+                return left_majority
+            else:
+                return right_majority
+        return divide(0,len(nums)-1)
         
