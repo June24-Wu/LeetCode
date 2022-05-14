4b00bf26395e997ce23495ee1a4e1646eeacd478
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-05-14 13:46:00 +0800

    Time: 711 ms (38.69%), Space: 34.4 MB (68.93%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -0,0 +1,30 @@
+class UF:
+    def __init__(self,n):
+        self.li = [i for i in range(n)]
+        self.setCount = n
+    def find(self,x):
+        if self.li[x] != x:
+            self.li[x] = self.find(self.li[x])
+        return self.li[x]
+    def union(self,a,b):
+        a = self.find(a)
+        b = self.find(b)
+        if a != b:
+            self.setCount -= 1
+        self.li[b] = a
+        return
+        
+
+class Solution:
+    def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        connections.sort(key = lambda x:(x[0],x[1]))
+        if len(connections) < n-1:return -1
+        uf = UF(n)
+        for i , j in connections:
+            uf.union(i,j)
+        print(uf.li)
+        print(uf.setCount)
+        # ans = set(uf.li)
+        return uf.setCount - 1
+        
+        
