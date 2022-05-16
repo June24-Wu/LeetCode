ef20e52d867ba66738e3c5523cd544857d6270ba
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-05-16 16:48:00 +0800

    Time: 49 ms (28.44%), Space: 13.9 MB (21.33%) - LeetHub

---
+++ 394-decode-string/394-decode-string.py
@@ -0,0 +1,28 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        ans = ""
+        num = ""
+        for i in s:
+            if i.isdigit():
+                num += i
+            elif i == "[":
+                stack.append(num)
+                num = ""
+                stack.append(i)
+            elif i == "]":
+                char = ""
+                while stack[-1] != "[":
+                    char = stack.pop() + char
+                stack.pop()
+                cnt = int(stack.pop())
+                char = cnt * char
+                stack.append(char)
+            else:
+                stack.append(i)
+            print(stack)
+        for i in stack:
+            ans += i
+        print(ans)
+        return ans
+        
