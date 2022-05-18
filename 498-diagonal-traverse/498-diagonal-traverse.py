edf7580cd2614f8e729724b6cbfc283154bcac8b
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-05-18 19:04:21 +0800

    Time: 290 ms (33.40%), Space: 17.6 MB (67.47%) - LeetHub

---
+++ 498-diagonal-traverse/498-diagonal-traverse.py
@@ -0,0 +1,44 @@
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        i , j = 0 , 0
+        m = len(mat) ; n = len(mat[0])
+        ans = []
+        flag = True
+        
+        def downToUp(i,j,mat):
+            nonlocal n
+            temp = []
+            while i >= 0  and j <= n - 1:
+                temp.append(mat[i][j])
+                i -= 1
+                j += 1
+            return temp
+        def upToDown(i,j,mat):
+            nonlocal n
+            temp = []
+            while i >= 0  and j <= n - 1:
+                temp.insert(0,mat[i][j])
+                i -= 1
+                j += 1
+            return temp
+        
+        while i <= m- 1 and j <= n-1:
+            if flag == True:
+                ans.extend(downToUp(i,j,mat))
+            else:
+                ans.extend(upToDown(i,j,mat))
+            if i != m - 1:
+                i+= 1
+            else:
+                j += 1
+            flag = not flag
+        print(ans)
+        return ans
+            
+                
+        
+        
+        
+        
+              
+        
