47dba9668edd0e9f43cb5d655f3a0df59f74b447
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-05-24 15:22:33 +0800

    Time: 4377 ms (5.01%), Space: 29.6 MB (86.36%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -1,29 +1,30 @@
-class Deque:
+
+class PQ:
     def __init__(self):
-        self.queue = []
+        self.li = []
+    def add(self,val):
+        while self.li != [] and val > self.li[-1]:
+            self.li.pop()
+        self.li.append(val)
     def pop(self,val):
-        if self.queue != [] and self.queue[0] == val:
-            self.queue.pop(0)
-        return
-    def push(self,val):
-        while self.queue != [] and val > self.queue[-1] :
-            self.queue.pop()
-        self.queue.append(val)
-        return
-    def get_front(self):
-        return self.queue[0]
+        if val == self.li[0]:
+            self.li.pop(0)
+    def getHead(self):
+        return self.li[0]
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        queue = Deque()
-        return_li = []
+        pq = PQ()
+        ans = []
         for i in range(k):
-            queue.push(nums[i])
-        return_li.append(queue.get_front())
+            pq.add(nums[i])
+        ans.append(pq.getHead())
         for i in range(k,len(nums)):
-            queue.pop(nums[i-k])
-            queue.push(nums[i])
-            return_li.append(queue.get_front())
-        return return_li
+            pq.pop(nums[i-k])
+            pq.add(nums[i])
+            ans.append(pq.getHead())
+        return ans
+            
+
             
         
         
