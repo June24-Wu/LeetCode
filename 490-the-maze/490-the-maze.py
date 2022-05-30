5123b53d224a127f7cc6481faff61991076b874e
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-05-30 14:06:08 +0800

    Time: 293 ms (85.11%), Space: 14.7 MB (42.65%) - LeetHub

--- 490-the-maze/490-the-maze.py
+++ 490-the-maze/490-the-maze.py
@@ -1,18 +1,21 @@
 class Solution:
     def hasPath(self, maze: List[List[int]], start: List[int], destination: List[int]) -> bool:
-        m, n, visit = len(maze), len(maze[0]), set()
-        directions = [(1, 0), (-1, 0), (0, -1), (0, 1)]
-        stack = [start]
-        while stack:
-            curx, cury = stack.pop()
-            if [curx, cury] == destination:
-                    return True
-            for dirx, diry in directions:
-                tx, ty = curx, cury
-                while 0 <= tx + dirx < m and 0 <= ty + diry < n and not maze[tx + dirx][ty + diry]:
-                    tx, ty = tx + dirx, ty + diry
-                if (tx, ty) not in visit:
-                    visit.add((tx, ty))
-                    stack.append((tx, ty))
+        
+        queue = [(start[0],start[1])]
+        visited = set()
+        m = len(maze) ; n = len(maze[0])
+        directions = [(1,0),(-1,0),(0,-1),(0,1)]
+        while queue != []:
+            curr = queue.pop()
+            if [curr[0],curr[1]] == destination:
+                return True
+            for i , j in directions:
+                x = curr[0] ; y = curr[1]
+                while 0 <= x < m and 0 <= y < n and maze[x][y] == 0:
+                    x += i ; y +=j
+                x -= i ; y -= j
+                if (x,y) not in visited:
+                    queue.append((x,y))
+                    visited.add((x,y))
         return False
                 
