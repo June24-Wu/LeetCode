4aaee26f8087d2cb6423b70f251a512618256e6b
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-05-31 20:05:21 +0800

    Time: 415 ms (5.02%), Space: 15.7 MB (34.45%) - LeetHub

---
+++ 224-basic-calculator/224-basic-calculator.py
@@ -0,0 +1,26 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        s.replace(" ","")
+        stack = []
+        res , num , sign =  0 , 0 , 1
+        for ch in s:
+            print(num, " ",ch)
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+            elif ch == "+" or ch == "-":
+                res += num * sign
+                num = 0
+                sign = 1 if ch == "+" else -1
+            elif ch == "(":
+                stack.append(res)
+                stack.append(sign)
+                res , num , sign =  0 , 0 , 1
+            elif ch == ")":
+                res += sign * num
+                num = 0 ; sign = 1
+                res*= stack.pop()
+                res += stack.pop()
+        res += sign * num
+        return res
+                
+        
