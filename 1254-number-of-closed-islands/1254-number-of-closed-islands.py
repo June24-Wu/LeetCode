f1760b7bc01218b5f20a8d8e3572dea33aaebc3a
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-06-02 19:10:45 +0800

    Time: 230 ms (24.50%), Space: 14.6 MB (46.54%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.py
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.py
@@ -1,26 +1,24 @@
 class Solution:
     def closedIsland(self, grid: List[List[int]]) -> int:
-        def dfs(grid,row,col):
-            if row < 0 or row >= m or col < 0 or col >= n:
-                return
-            if grid[row][col] == 1:
+        m = len(grid) ; n = len(grid[0])
+        
+        def dfs(row,col):
+            if row < 0 or row >= m or col < 0 or col >= n or grid[row][col] == 1:
                 return
             grid[row][col] = 1
-            dfs(grid,row-1,col)
-            dfs(grid,row+1,col)
-            dfs(grid,row,col+1)
-            dfs(grid,row,col-1) 
-            return
-        m = len(grid) ; n = len(grid[0])
-        for i in range(n):
-            dfs(grid,m-1,i)
-            dfs(grid,0,i)
+            for i , j in [(1,0),(0,1),(-1,0),(0,-1)]:
+                dfs(row+i,col+j)
+            
         for i in range(m):
-            dfs(grid,i,0) ; dfs(grid,i,n-1)
-        cnt = 0
+            dfs(i,0)
+            dfs(i,n-1)
+        for i in range(n):
+            dfs(0,i)
+            dfs(m-1,i)
+        ans = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 0:
-                    cnt += 1
-                    dfs(grid,i,j)
-        return cnt
+                    ans += 1
+                    dfs(i,j)
+        return ans
