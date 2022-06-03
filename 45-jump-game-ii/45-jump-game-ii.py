62a792b40bc786fd8818b4ce694de909677550a3
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-06-03 18:40:46 +0800

    Time: 191 ms (59.83%), Space: 15.2 MB (24.04%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,12 +1,36 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        dp = [float("inf") for _ in range(len(nums))]
-        dp[0] = 0
-        for index , num in enumerate(nums):
-            for j in range(index+1,index + num + 1):
-                if j >= len(nums):
-                    return dp[-1]
-                dp[j] = min(dp[j],dp[index] + 1)
-            # print(dp)
-        return dp[-1]
+        # Greedy
+        if len(nums) == 1:
+            return 0
+        jump = 0 ; right = 0 ; cnt = 0
+        
+        for i in range(len(nums) - 1):
+            jump = max(jump,nums[i] + i)
+            if i == right:
+                cnt += 1
+                right = jump
+        return cnt
+                
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # DP
+        # dp = [float("inf") for _ in range(len(nums))]
+        # dp[0] = 0
+        # for index , num in enumerate(nums):
+        #     for j in range(index+1,index + num + 1):
+        #         if j >= len(nums):
+        #             return dp[-1]
+        #         dp[j] = min(dp[j],dp[index] + 1)
+        # return dp[-1]
             
