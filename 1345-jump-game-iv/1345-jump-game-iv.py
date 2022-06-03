adb40c30de0a07ed6df84b7f9d3b84149e219f6d
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-06-03 19:56:26 +0800

    Time: 3177 ms (5.03%), Space: 27.4 MB (89.05%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.py
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -5,9 +5,33 @@ class Solution:
             if num not in table:
                 table[num] = []
             table[num].append(index)
-        print(table)
-        
-        
+        # arrLen = len(arr)
+        # top = [(0,arr[0])] ; down = [(arrLen -1,arr[-1])]  # index , val
+        # arr[0] = None ; arr[-1] = None;cnt = 0
+        # while top:
+        #     print(top,down)
+        #     if len(top) > len(down):
+        #         top , down = down , top
+        #     length = len(top)
+        #     for _ in range(length):
+        #         index , val = top.pop(0)
+        #         if (index,val) in down:
+        #             return cnt
+        #         left = index - 1 ; right = index + 1
+        #         if left >= 0 and arr[left] != None:
+        #             top.append((left,arr[left]))
+        #             arr[left] = None
+        #         if right < len(arr) and arr[right] != None:
+        #             top.append((right,arr[right]))
+        #             arr[right] = None
+        #         while table[val]:
+        #             subIndex = table[val].pop()
+        #             if subIndex == index:
+        #                 continue
+        #             if arr[subIndex] != None:
+        #                 top.append((subIndex,val))
+        #     cnt += 1
+        # return -1
         queue = [(0,arr[0])]
         arr[0] = None ; cnt = 0
         while queue:
