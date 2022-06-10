586169fa0c89a1f0f8b767e47444eaabdebe29a4
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-06-10 15:29:38 +0800

    Time: 228 ms (5.04%), Space: 14.6 MB (42.78%) - LeetHub

---
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.py
@@ -0,0 +1,33 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        graph = {}
+        for index  , (start , end) in enumerate(tickets):
+            if start not in graph:
+                graph[start] = []
+            graph[start].append((end,index))
+        print(graph)
+        for i in graph:
+            graph[i].sort()
+        visited = [False for _ in range(len(tickets))]
+        start = "JFK"
+        ans = None
+        def backtracking(start,path):
+            nonlocal ans
+            if ans != None:
+                return
+            if all(visited):
+                ans = path[:]
+                return
+            if start not in graph:
+                return
+            for end , index in graph[start]:
+                if visited[index] == True:
+                    continue
+                visited[index] = True
+                path.append(end)
+                backtracking(end,path)
+                visited[index] = False
+                path.pop()
+        backtracking("JFK",["JFK"])
+        return ans
+        
