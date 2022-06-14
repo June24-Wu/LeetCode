de4cfeaaeb6746443aea5415729f55af98f2b1c6
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-06-14 20:36:38 +0800

    Time: 804 ms (28.11%), Space: 105.7 MB (19.36%) - LeetHub

--- 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
@@ -3,13 +3,14 @@ class Solution:
         
         mod = 10 ** 9 + 7
         
-        @lru_cache(None)
+        memo = {}
         def dfs(idx, step):
             if idx == 0 and step == 0:
                 return 1
             if step == 0:
                 return 0
-            
+            if (idx,step) in memo:
+                return memo[(idx,step)]
 			# option 1: stay
             ans = dfs(idx, step - 1)
             if idx + 1 < arrLen:
@@ -19,7 +20,7 @@ class Solution:
             if idx - 1 >= 0:
 				# option 3: to the left direction
                 ans += dfs(idx - 1, step - 1)
-                
+            memo[(idx,step)] = ans % mod
             return ans % mod
         
         return dfs(0, steps)
