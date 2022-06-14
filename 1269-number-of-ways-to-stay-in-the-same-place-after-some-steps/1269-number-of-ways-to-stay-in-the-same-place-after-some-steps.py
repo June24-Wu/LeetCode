8b9a32f92700ab15d30b5fade15e85e24e49005a
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-06-14 20:34:05 +0800

    Time: 951 ms (17.98%), Space: 104.5 MB (21.20%) - LeetHub

---
+++ 1269-number-of-ways-to-stay-in-the-same-place-after-some-steps/1269-number-of-ways-to-stay-in-the-same-place-after-some-steps.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numWays(self, steps: int, arrLen: int) -> int:
+        
+        mod = 10 ** 9 + 7
+        
+        @lru_cache(None)
+        def dfs(idx, step):
+            if idx == 0 and step == 0:
+                return 1
+            if step == 0:
+                return 0
+            
+			# option 1: stay
+            ans = dfs(idx, step - 1)
+            if idx + 1 < arrLen:
+				# option 2: to the right direction
+                ans += dfs(idx + 1, step - 1)
+            
+            if idx - 1 >= 0:
+				# option 3: to the left direction
+                ans += dfs(idx - 1, step - 1)
+                
+            return ans % mod
+        
+        return dfs(0, steps)
