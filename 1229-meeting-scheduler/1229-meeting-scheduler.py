834660b0d24146adc89f3898a92700d2d33828d2
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-06-28 19:59:08 +0800

    Time: 1165 ms (9.04%), Space: 24 MB (6.14%) - LeetHub

--- 1229-meeting-scheduler/1229-meeting-scheduler.py
+++ 1229-meeting-scheduler/1229-meeting-scheduler.py
@@ -1,19 +1,23 @@
 class Solution:
-    def minAvailableDuration(self, slots1: List[List[int]], slots2: List[List[int]], duration: int) -> List[int]:
-
-        slots1.sort()
-        slots2.sort()
-        pointer1 = pointer2 = 0
-
-        while pointer1 < len(slots1) and pointer2 < len(slots2):
-            # find the boundaries of the intersection, or the common slot
-            intersect_right = min(slots1[pointer1][1], slots2[pointer2][1])
-            intersect_left = max(slots1[pointer1][0],slots2[pointer2][0])
-            if intersect_right - intersect_left >= duration:
-                return [intersect_left, intersect_left + duration]
-            # always move the one that ends earlier
-            if slots1[pointer1][1]< slots2[pointer2][1]:
-                pointer1 += 1
-            else:
-                pointer2 += 1
+    def minAvailableDuration(self, s1: List[List[int]], s2: List[List[int]], duration: int) -> List[int]:
+        arr = []
+        for s , e in s1:
+            arr.append((s,1))
+            arr.append((e, - 1))
+        for s , e in s2:
+            arr.append((s,1))
+            arr.append((e, - 1))
+        arr.sort()
+        
+        flag = False ; left = 0 ; cnt = 0
+        for idx , val in arr:
+            cnt += val
+            if not flag and cnt == 2:
+                flag = True
+                left = idx
+            if flag and cnt < 2:
+                flag = False
+                if idx - left >= duration:
+                    return [left,left + duration]
         return []
+        
