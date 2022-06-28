80f065fb624f44166b3a985f63c88382a5f82a06
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-06-28 20:04:01 +0800

    Time: 200 ms (61.05%), Space: 15.4 MB (15.04%) - LeetHub

--- 986-interval-list-intersections/986-interval-list-intersections.py
+++ 986-interval-list-intersections/986-interval-list-intersections.py
@@ -1,22 +1,20 @@
 class Solution:
-    def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
-        p1 = 0 ; p2 = 0
-        rt = []
-        while p1 < len(firstList) and p2 < len(secondList):
-            left = max(firstList[p1][0],secondList[p2][0])
-            right = min(firstList[p1][1],secondList[p2][1])
-            
-            if left <= right:
-                rt.append([left,right])
-                if firstList[p1][1] < secondList[p2][1]:
-                    p1 += 1
-                else:
-                    p2 += 1
-                continue
-            elif firstList[p1][1] < secondList[p2][0]:
-                p1 += 1
-            else:
-                p2 += 1
-        return rt
-                
-        
+    def intervalIntersection(self, l1: List[List[int]], l2: List[List[int]]) -> List[List[int]]:
+        arr = []
+        for s , e in l1:
+            arr.append((s,1))
+            arr.append((e,-1))
+        for s , e in l2:
+            arr.append((s,1))
+            arr.append((e,-1)) 
+        arr.sort(key = lambda x : (x[0], - x[1]))
+        flag = False ; cnt = 0 ; left = 0 ; ans = []
+        for idx , val in arr:
+            cnt += val
+            if flag == False and cnt == 2:
+                left = idx
+                flag = True
+            if flag and cnt < 2:
+                ans.append([left,idx])
+                flag = False
+        return ans
