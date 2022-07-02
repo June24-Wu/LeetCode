183e283a0c57d5a0d987da1f3c3cfb68308c65fd
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-07-02 11:27:05 +0800

    Time: 203 ms (17.88%), Space: 15.5 MB (88.20%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,28 +1,54 @@
 class Solution:
     def canFinish(self, n: int, prerequisites: List[List[int]]) -> bool:
         visited = [False] * n
-        path = [False] * n
         graph = collections.defaultdict(list)
+        indegree = [0] * n
         for i , j in prerequisites:
-            if j not in graph:
-                graph[j] = []
             graph[j].append(i)
-        flag = True
-        print(graph)
-        def dfs(index):
-            nonlocal flag ; nonlocal path ; nonlocal visited
-            if path[index]:
-                flag = False
-                return
-            if visited[index] or flag == False:
-                return
-            visited[index] = True
-            path[index] = True
-            for next in graph[index]:
-                dfs(next)
-            path[index] = False
-            return
-        for i in range(n):
-            dfs(i)
-        return flag
+            indegree[i] += 1
+        queue = []
+        for i in range(len(indegree)):
+            if indegree[i] == 0:
+                queue.append(i)
+        while queue:
+            node = queue.pop(0)
+            visited[node] = True
+            for next in graph[node]:
+                if visited[next]:
+                    return False
+                indegree[next] -= 1
+                if indegree[next] == 0:
+                    queue.append(next)
+        
+        return max(indegree) == 0
+        
+        
+        
+        
+        # DFS
+        # visited = [False] * n
+        # path = [False] * n
+        # graph = collections.defaultdict(list)
+        # for i , j in prerequisites:
+        #     if j not in graph:
+        #         graph[j] = []
+        #     graph[j].append(i)
+        # flag = True
+        # print(graph)
+        # def dfs(index):
+        #     nonlocal flag ; nonlocal path ; nonlocal visited
+        #     if path[index]:
+        #         flag = False
+        #         return
+        #     if visited[index] or flag == False:
+        #         return
+        #     visited[index] = True
+        #     path[index] = True
+        #     for next in graph[index]:
+        #         dfs(next)
+        #     path[index] = False
+        #     return
+        # for i in range(n):
+        #     dfs(i)
+        # return flag
         
