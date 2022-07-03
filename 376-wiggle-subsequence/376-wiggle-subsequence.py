0cdd041a8d5bb06f93a58220569d932c1129d237
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-07-03 09:26:58 +0800

    Time: 42 ms (80.10%), Space: 13.9 MB (77.55%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.py
+++ 376-wiggle-subsequence/376-wiggle-subsequence.py
@@ -1,11 +1,34 @@
 class Solution:
     def wiggleMaxLength(self, nums: List[int]) -> int:
         up = [0] * len(nums)
-        down = [0] * len(nums)      
-        for i in range(len(nums)):
-            for j in range(i-1,-1,-1):
-                if nums[i] > nums[j]:
-                    up[i] = max(up[i],down[j] + 1)
-                if nums[i] < nums[j]:
-                    down[i] = max(down[i],up[j] + 1)
-        return max(up[-1],down[-1]) + 1
+        down = [0] * len(nums)
+        
+        for i in range(1,len(nums)):
+            if nums[i] > nums[i-1]:
+                up[i] = down[i-1] + 1
+                down[i] = down[i-1]
+            elif nums[i] < nums[i-1]:
+                down[i] = up[i-1] + 1
+                up[i] = up[i-1]
+            else:
+                up[i] = up[i-1]
+                down[i] = down[i-1]
+        return max(down[-1],up[-1]) + 1
+         
+        
+        
+        
+        
+        
+        
+        
+        # O(n2)
+        # up = [0] * len(nums)
+        # down = [0] * len(nums)      
+        # for i in range(len(nums)):
+        #     for j in range(i-1,-1,-1):
+        #         if nums[i] > nums[j]:
+        #             up[i] = max(up[i],down[j] + 1)
+        #         if nums[i] < nums[j]:
+        #             down[i] = max(down[i],up[j] + 1)
+        # return max(up[-1],down[-1]) + 1
