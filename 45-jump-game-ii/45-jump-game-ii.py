4c290f75c6d34a6238785a3dbaed9f4f9f05105b
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-07-05 09:06:58 +0800

    Time: 10013 ms (15.68%), Space: 14.9 MB (89.59%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.py
+++ 45-jump-game-ii/45-jump-game-ii.py
@@ -1,36 +1,11 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        # Greedy
-        if len(nums) == 1:
-            return 0
-        jump = 0 ; right = 0 ; cnt = 0
-        
-        for i in range(len(nums) - 1):
-            jump = max(jump,nums[i] + i)
-            if i == right:
-                cnt += 1
-                right = jump
-        return cnt
-                
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # DP
-        # dp = [float("inf") for _ in range(len(nums))]
-        # dp[0] = 0
-        # for index , num in enumerate(nums):
-        #     for j in range(index+1,index + num + 1):
-        #         if j >= len(nums):
-        #             return dp[-1]
-        #         dp[j] = min(dp[j],dp[index] + 1)
-        # return dp[-1]
-            
+        n = len(nums)
+        dp = [float("inf")] * n
+        dp[0] = 0
+        for i in range(n):
+            step = nums[i]
+            for j in range(i+1,i+step + 1):
+                if j < n:
+                    dp[j] = min(dp[j],dp[i] + 1)
+        return dp[-1]
