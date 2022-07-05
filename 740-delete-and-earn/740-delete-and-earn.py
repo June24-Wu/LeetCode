df71197d8a80b6c33b94d742b6014ef74cf44d56
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-07-05 08:52:24 +0800

    Time: 2338 ms (5.01%), Space: 16.5 MB (37.96%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.py
+++ 740-delete-and-earn/740-delete-and-earn.py
@@ -1,21 +1,17 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        table = {i : nums.count(i) for i in set(nums)}
-        nums = sorted(set(nums))
-        print(nums)
-        dp = [0 for _ in range(len(nums))]
+        table = {val : nums.count(val) for val in set(nums)}
+        nums = list(set(nums))
+        nums.sort()
+        dp = [0] * len(nums)
         dp[0] = nums[0] * table[nums[0]]
-        
-        def get(n):
-            if n >= 0:
-                return dp[n]
-            return 0
         for i in range(1,len(nums)):
-            num = nums[i] ; cnt = table[num]
-            if nums[i] - nums[i-1] == 1:
-                dp[i] = max(num*cnt+get(i-2),dp[i-1])
+            curr = nums[i] * table[nums[i]]
+            if nums[i] - nums[i - 1] == 1:
+                curr = max(curr + dp[i-2],dp[i-1])
             else:
-                dp[i] = dp[i-1] + num*cnt
-        # print(dp)
-        
+                curr += dp[i - 1]
+            dp[i] = curr
+
         return dp[-1]
+        
