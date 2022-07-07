a1b0c79367c96a961d7df7d9677317645b5994db
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-07-07 09:32:45 +0800

    Time: 34 ms (90.60%), Space: 13.9 MB (71.29%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.py
+++ 213-house-robber-ii/213-house-robber-ii.py
@@ -1,38 +1,13 @@
 class Solution:
-    
-    
-    def __rob(self,num):
-            if len(num) <= 2:return max(num)
-            num[1] = max(num[0:2])
-            for i in range(2,len(num)):
-                num[i] = max(num[i-1],num[i]+num[i-2])
-            return num[-1]
     def rob(self, nums: List[int]) -> int:
-        if len(nums) <= 3: return max(nums)
-        return max(self.__rob(nums[:len(nums)-1]),self.__rob(nums[1:]))
-        # return 3
-        
-        
-        
-        
-        
-        
-    #     if nums == None or nums == []: return 0
-    #     length = len(nums)
-    #     if length == 1:return nums[0]
-    #     dp = [0for _ in range(length)]
-    #     dp[0] = nums[0]
-    #     dp[1] = max(nums[0],nums[1])
-    #     for i in range(2,length):
-    #         dp[i] = max(dp[i-1],dp[i-2]+nums[i])
-    #     return dp[-1]
-    # def rob(self, nums: List[int]) -> int:
-    #     if nums == [] or not nums: return 0
-    #     length = len(nums)
-    #     if length == 1:return nums[0]
-    #     return max(self.__rob(nums[0:length-1]),self.__rob(nums[1:length]))
-    
-    
-    
-    
+        if len(nums) == 1:
+            return nums[0]
+        def __rob(array):
+            if len(array) == 1:
+                return array[0]
+            array[1] = max(array[0],array[1])
+            for i in range(2,len(array)):
+                array[i] = max(array[i - 1] , array[i] + array[i - 2])
+            return array[-1]
+        return max(__rob(nums[:-1]), __rob(nums[1:]))
         
