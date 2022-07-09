3e4dd0685c93a7031377a5788492c80617d6d9f4
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-07-09 09:32:16 +0800

    Time: 2235 ms (13.37%), Space: 34.6 MB (23.74%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.py
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -12,16 +12,4 @@ class Solution:
             heapq,heappush(heap,(-val,i))
         return ans
         
-   
-        # BF DP 做法： TLE
-        n = len(nums)
-        dp = [ - float("inf") for _ in range(n)]
-        dp[0] = nums[0]
-        for index , num in enumerate(nums):
-            for j in range(1,1 + k):
-                if index + j < n:
-                    dp[index+j] = max(dp[index+j],dp[index] + nums[index + j])
-            # print(index)
-            # print(dp)
-        return dp[-1]
         
