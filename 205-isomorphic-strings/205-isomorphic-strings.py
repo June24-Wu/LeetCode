a44f853d78cae1794d77da3e70548506d4d21c2f
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-07-10 08:38:07 +0800

    Time: 64 ms (53.82%), Space: 14.1 MB (88.81%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.py
+++ 205-isomorphic-strings/205-isomorphic-strings.py
@@ -1,25 +1,19 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        left, right = [[] for _ in range(len(s))] , [[] for _ in range(len(t))]
-        table1 , table2 = {} , {}
-        index = 0
+        table = {}
         for i in range(len(s)):
-            ch = s[i]
-            if ch not in table1.keys():
-                table1[ch] = index
-                left[index].append(i)
-                index += 1
+            if s[i] not in table:
+                table[s[i]] = t[i]
             else:
-                left[table1[ch]].append(i)
-        index = 0
-        for i in range(len(t)):
-            ch = t[i]
-            if ch not in table2.keys():
-                table2[ch] = index
-                right[index].append(i)
-                index += 1
+                if table[s[i]] != t[i]:
+                    return False
+        table = {}
+        for i in range(len(s)):
+            if t[i] not in table:
+                table[t[i]] = s[i]
             else:
-                right[table2[ch]].append(i)
-        return left == right
-            
+                if table[t[i]] != s[i]:
+                    return False
+                        
+        return True
         
