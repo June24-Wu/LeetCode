59558ebe7b6472b7e713474a88cdf5d5c456a26b
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-07-13 09:05:20 +0800

    Time: 48 ms (58.57%), Space: 14 MB (22.22%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -1,16 +1,18 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        mid , max_even = None,0
-        table = {}
-        for i in s:
-            table[i] = table[i] + 1 if i in table.keys() else 1
-            value = table[i]
-            if value % 2 == 0:
-                max_even += 2
-        for i in s:
-            if table[i] % 2 == 1:
-                return max_even + 1
-        return max_even
-                
-        
+        array = [(i,s.count(i)) for i in set(list(s))]
+        array.sort(key = lambda x : - x[1])
+        print(array)
+        maxodd = None
+        ans = 0
+        for char , cnt in array:
+            if cnt % 2 == 0:
+                ans += cnt
+            if cnt % 2 == 1:
+                if maxodd == None:
+                    maxodd = char
+                    ans += cnt
+                else:
+                    ans += cnt - 1
+        return ans
         
