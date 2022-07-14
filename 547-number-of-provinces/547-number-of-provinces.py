1a5fad76fa8bc1dfdb53dc166e6c8662c6c5138d
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-07-14 08:35:13 +0800

    Time: 1943 ms (5.01%), Space: 14.6 MB (29.64%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.py
+++ 547-number-of-provinces/547-number-of-provinces.py
@@ -1,24 +1,30 @@
+class UF:
+    def __init__(self,n):
+        self.parents = [i for i in range(n)]
+        self.cnt = n
+    def find(self,index):
+        if self.parents[index] != index:
+            self.parents[index] = self.find(self.parents[index])
+        return self.parents[index]
+    def union(self,a,b):
+        a = self.find(a)
+        b = self.find(b)
+        if a != b:
+            self.cnt -= 1
+            self.parents[b] = a
+        return
+        
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        length = len(isConnected)
-        visited = [False for _ in range(length)]
-        
-        def dfs(index):
-            nonlocal visited
-            if visited[index] == True:
-                return
-            visited[index] = True
-            for i in range(len(isConnected[index])):
-                if i != index and isConnected[index][i] == 1:
-                    dfs(i)
-            return
-        
-        
-        cnt = 0
-        for i in range(len(isConnected)):
-            if visited[i] == False:
-                dfs(i)
-                cnt += 1
-        return cnt
+        m = len(isConnected) ; n = len(isConnected[0])
+        a = []
+        for i in range(m):
+            for j in range(n):
+                if i != j and isConnected[i][j] == 1 and [j,i] not in a:
+                    a.append([i,j])
+        uf = UF(m)
+        for i , j in a:
+            uf.union(i,j)
+        return uf.cnt
             
         
