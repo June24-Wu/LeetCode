7c450b5a83d4540a4b94b99b013a0f826d763f63
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-07-14 08:26:06 +0800

    Time: 78 ms (7.26%), Space: 14.3 MB (51.36%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.py
@@ -6,46 +6,20 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if root == None:return root
-        final = []
-        sub_final = []
-        stack = [root]
-        cnt = 0
-        size = len(stack)
+        ans = []
+        if not root:
+            return ans
+        queue = [root]
         
-        # --------1
-        # while stack != []:
-        #     sub_final = []
-        #     for i in stack:
-        #         sub_final.append(i.val)
-        #     final.append(sub_final)
-        #     new_stack = stack.copy()
-        #     stack = []
-        #     while new_stack != []:
-        #         a = new_stack.pop(0)
-        #         if a.left != None:
-        #             stack.append(a.left)
-        #         if a.right != None:
-        #             stack.append(a.right)
-        # return final
-        
-        
-        # --------2
-        while stack != []:
-            a = stack.pop(0)
-            sub_final.append(a.val)
-            if a.left != None:
-                stack.append(a.left)
-            if a.right != None:
-                stack.append(a.right)
-            cnt += 1
-            if cnt == size:
-                final.append(sub_final)
-                sub_final = []
-                size = size = len(stack)
-                cnt = 0
-        return final
-            
-                
-            
-        
+        while queue:
+            length = len(queue)
+            sub = []
+            for _ in range(length):
+                node = queue.pop(0)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                sub.append(node.val)
+            ans.append(sub)
+        return ans
