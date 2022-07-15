e4ceb350fb39dbc69c764c4bcd93949de610ccb3
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-07-15 19:46:29 +0800

    Time: 41 ms (86.38%), Space: 13.9 MB (84.27%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.py
+++ 486-predict-the-winner/486-predict-the-winner.py
@@ -1,13 +1,11 @@
 class Solution:
     def PredictTheWinner(self, nums: List[int]) -> bool:
-        memo = {}
-        
-        def dfs(left,right):
-            if (left,right) in memo:
-                return memo[(left,right)]
-            if left == right:
-                memo[(left,right)] = nums[left]
-                return nums[left]
-            return max(nums[left] - dfs(left+1,right) , nums[right] - dfs(left,right - 1))
-        return dfs(0,len(nums) - 1) >= 0
+        n = len(nums)
+        dp = [[ 0 for i in range(n)] for i in range(n)]
+        for i in range(n):
+            dp[i][i] = nums[i]
+        for i in range(n - 1,-1,-1):
+            for j in range(i+1,n):
+                dp[i][j] = max(nums[i] - dp[i+1][j] , nums[j] - dp[i][j-1])
+        return dp[0][n-1] >= 0
         
