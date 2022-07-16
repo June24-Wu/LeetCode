957e672b2e0eed3e7dbfad1d9fb2ae4cfa6a3690
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-07-16 10:35:35 +0800

    Time: 401 ms (64.96%), Space: 15.8 MB (50.43%) - LeetHub

--- 1140-stone-game-ii/1140-stone-game-ii.py
+++ 1140-stone-game-ii/1140-stone-game-ii.py
@@ -1,28 +1,20 @@
 class Solution:
     def stoneGameII(self, piles: List[int]) -> int:
         n = len(piles)
-        dp = {} 
-        def recursion(index,M):
-            # if we reached to the end we cannot score any value
-            if index == n:
-                return 0
-            # we search if we have solved the same case earlier
-            if (index,M) in dp:
-                return dp[(index,M)] 
-            # total remaining score is the sum of array from index to the end
-            total = sum(piles[index:])           
-            # if we can take the complete array it is the best choice
-            if index + 2*M >= n :return total
-            # my_score is the score we are getting as the player who is playing
-            my_score = 0
-            for x in range(index,index+2*M):
-                # opponent score will be calculated by next recursion
-                opponent_score = recursion(x+1,max(M,x-index+1))
-                # my_score is the remaining value of total - opponent_score
-                my_score = max(my_score,total - opponent_score)          
-            # this is memoization part
-            dp[(index,M)] = my_score
-            # return the score
-            return my_score
+        memo = {}
         
-        return recursion(0,1)
+        def dfs(index,m):
+            nonlocal memo
+            if (index,m) in memo:
+                return memo[(index,m)]
+            if 2*m >= n - index:
+                memo[(index,m)] = sum(piles[index:])
+                return sum(piles[index:])
+            res =  - float("inf")
+            for i in range(1,2*m + 1):
+                res = max(res,sum(piles[index:index+i]) - dfs(index+i,max(m,i)))
+            memo[(index,m)] = res
+            return res
+        ans = (dfs(0,1) + sum(piles)) // 2
+        # print(memo)
+        return ans
