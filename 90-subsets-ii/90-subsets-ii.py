070b04cf6689ef9e59cec72babb1920d081e5904
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-07-17 10:12:04 +0800

    Time: 41 ms (88.75%), Space: 14.2 MB (48.82%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,35 +1,18 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        return_li = [[]]
         nums.sort()
-        length = len(nums)
-        def backtracking(start_index,path):
-            if start_index == length:
+        ans = []
+        def bt(index,path):
+            if index > len(nums):
                 return
-            for i in range(start_index,length):
-                if i > start_index and nums[i] == nums[i-1]:
+            ans.append(path[:])
+            for i in range(index,len(nums)):
+                if i > index and nums[i] == nums[i - 1]:
                     continue
                 path.append(nums[i])
-                return_li.append(path[:])
-                backtracking(i+1,path)
+                bt(i+1,path)
                 path.pop()
-        backtracking(0,[])
-        return return_li
-            
-    
-#         nums.sort()
-#         q = [[]]
-#         length = len(nums)
-#         for i in nums:
-#             for j in range(len(q)):
-#                 q.append(q[j] + [i])
+            return
+        bt(0,[])
+        return ans
         
-#         l2 = []
-#         for i in q:
-#             i = str(i).replace("[","").replace("]","")
-#             l2.append(i)
-#         l2 = list(set(l2))
-#         l3 = []
-#         for i in l2:
-#             l3.append(i.split(","))
-#         return l3
