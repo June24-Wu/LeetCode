db531bb2a3d5c90b1eb367755a4593adfffc008b
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-07-17 10:04:19 +0800

    Time: 339 ms (85.68%), Space: 16.4 MB (80.10%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,29 +1,20 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
         
-        def dfs(grid,row,col):
-            # boundary
-            if row < 0 or row >= m or col < 0 or col >=n:
-                return
-            if grid[row][col] == "0":
-                return
-            grid[row][col] = "0"
-            dfs(grid,row+1,col)
-            dfs(grid,row-1,col)
-            dfs(grid,row,col+1)
-            dfs(grid,row,col-1)
+        m = len(grid) ; 
+        n = len(grid[0])
+        def dfs(r,c):
+            grid[r][c] = 0
+            for i , j in [(1,0),(-1,0),(0,1),(0,-1)]:
+                row = r + i ; col = c + j
+                if 0 <= row < m and 0 <= col < n and grid[row][col] == "1":
+                    dfs(row,col)
             return
         cnt = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == "1":
                     cnt += 1
-                    dfs(grid,i,j)
+                    dfs(i,j)
         return cnt
-        
-        
-
-        
         
