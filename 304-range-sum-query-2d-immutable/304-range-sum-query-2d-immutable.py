fc8e155662fc52d9e4e6c0a01b74005c223f73b8
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-07-18 11:49:58 +0800

    Time: 1468 ms (97.33%), Space: 25.2 MB (27.28%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.py
@@ -1,17 +1,30 @@
 class NumMatrix:
 
-    def __init__(self, matrix: List[List[int]]):
-        self.matrix = [[0 for _ in range(len(matrix[0])+1)] for _ in range(len(matrix)+1)]
-        
-        for i in range(1,len(self.matrix)):
-            for j in range(1,len(self.matrix[0])):
-                self.matrix[i][j] = self.matrix[i-1][j] + self.matrix[i][j-1] + matrix[i-1][j-1] - self.matrix[i-1][j-1]
-        print(self.matrix)
-        
-        
-
+    def __init__(self, mat: List[List[int]]):
+        m = len(mat) ; n = len(mat[0])
+        preSum = [[0 for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    preSum[i][j] = mat[i][j]
+                elif i == 0:
+                    preSum[i][j] = preSum[i][j-1] +  mat[i][j]
+                elif j == 0:
+                    preSum[i][j] = preSum[i-1][j] +  mat[i][j]                    
+                    
+                else:
+                    preSum[i][j] = preSum[i-1][j] + preSum[i][j-1] - preSum[i-1][j-1] + mat[i][j]
+        self.preSum = preSum
+                
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return self.matrix[row2+1][col2+1] - self.matrix[row2+1][col1] - self.matrix[row1][col2+1] + self.matrix[row1][col1]
+        if row1 == 0 and col1 == 0:
+            return self.preSum[row2][col2]
+        elif row1 == 0:
+            return self.preSum[row2][col2] - self.preSum[row2][col1-1]
+        elif col1 == 0:
+            return self.preSum[row2][col2] - self.preSum[row1-1][col2]
+        else:
+            return self.preSum[row2][col2] - self.preSum[row2][col1-1] - self.preSum[row1-1][col2] + self.preSum[row1-1][col1-1]
         
 
 
