32323462d6e05caf956f6427cd7f47c269f9c53c
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-07-25 09:06:45 +0800

    Time: 332 ms (76.67%), Space: 32.9 MB (13.41%) - LeetHub

--- 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
+++ 583-delete-operation-for-two-strings/583-delete-operation-for-two-strings.py
@@ -1,18 +1,15 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        long = word1 if len(word1) >= len(word2) else word2
-        short = word1 if len(word1) < len(word2) else word2
+        m = len(word1) ; n  = len(word2)
         
-        dp = [0 for _ in range(len(long))]
+        @cache
+        def dfs(idx1,idx2):
+            if idx1 == m:
+                return n - idx2
+            if idx2 == n:
+                return m - idx1
+            if word1[idx1] != word2[idx2]:
+                return min(dfs(idx1+1,idx2)+1,dfs(idx1,idx2+1)+1,dfs(idx1+1,idx2+1) + 2)
+            return dfs(idx1+1,idx2+1)
+        return dfs(0,0)
         
-        
-        for item in short:
-            for j in range(len(dp)-1,-1,-1):
-                if long[j] == item:
-                    if j == 0:
-                        dp[j] = 1
-                    else:
-                        dp[j] = max(dp[:j]) + 1
-            print(dp)
-        length = max(dp)
-        return len(word1) + len(word2) - 2 * length
