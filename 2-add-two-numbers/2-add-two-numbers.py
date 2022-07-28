ebe147600e909ed634f778dddb863e77c827d94d
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-07-28 11:59:01 +0800

    Time: 134 ms (18.91%), Space: 14 MB (43.79%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,29 +5,17 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        p1 = l1 ; p2 = l2
-        l3 = ListNode(-1) ; p3 = l3
-        add = 0
-        while p1 != None or p2 != None:
-            if p1 == None:
-                p3.next = ListNode((p2.val + add) % 10)
-                add = (p2.val + add) // 10
-                p2 = p2.next ; p3 = p3.next
-            elif p2 == None:
-                p3.next = ListNode((p1.val + add) % 10)
-                add = (p1.val + add) // 10
-                p1 = p1.next ; p3 = p3.next
-            else:
-                p3.next = ListNode((p1.val + p2.val + add) % 10)
-                add = (p1.val + p2.val + add) // 10
-                p2 = p2.next ; p3 = p3.next ; p1 = p1.next
-        if add != 0:
-            p3.next = ListNode(add)
-                
-        return l3.next
-            
-            
-        
-        
-        
+        node = ListNode(-1)
+        curr = node
+        p1 = l1 ; p2 = l2 ; remain = 0
+        while p1 or p2:
+            val1 = p1.val if p1 else 0
+            val2 = p2.val if p2 else 0
+            val , remain = (val1 + val2 + remain) % 10 , (val1 + val2 + remain) // 10
+            curr.next = ListNode(val)
+            curr = curr.next
+            p1 = p1.next if p1 else p1
+            p2 = p2.next if p2 else p2
+        curr.next = ListNode(remain) if remain > 0 else None
+        return node.next
         
