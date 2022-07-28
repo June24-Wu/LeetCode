817e40ad22fed99550bf459901766b05fb68e6c4
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-07-28 18:44:49 +0800

    Time: 92 ms (28.96%), Space: 14.5 MB (34.60%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,10 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        
+        
         l1 = [0 for _ in range(26)]
         l2 = [0 for _ in range(26)]
-        for i in s:
+        for i in s :
             l1[ord(i) - ord("a")] += 1
         for i in t:
             l2[ord(i) - ord("a")] += 1
-        return l1 == l2 
+        return l1 == l2
+        
+
         
