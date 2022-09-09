328fdc9652895570965559d5b9540f642021e1d7
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-09-08 20:56:48 -0500

    Time: 4265 ms (18.42%), Space: 65.7 MB (97.40%) - LeetHub

---
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfWeakCharacters(self, mat: List[List[int]]) -> int:
+        mat.sort(key = lambda x : ( - x[0], x [1]))
+        ans = 0
+        left = 0 ; maxdefense = 0
+        print(mat)
+        for right in range(len(mat)):
+            maxdefense = max(maxdefense,mat[right][1])
+            if mat[left][0] == mat[right][0]:
+                left = right - 1
+            if mat[right][1] < maxdefense:
+                ans += 1
+        return ans
+            
