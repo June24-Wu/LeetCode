5548911c007ed09c124b3a01f048f874c281fe33
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-09-13 20:53:49 -0500

    Time: 2138 ms (8.42%), Space: 84.5 MB (91.28%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.java
@@ -1,47 +1,32 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    int ans = 0 ; 
-    HashMap <Integer , Integer> table = new HashMap<>();
-    public int pseudoPalindromicPaths (TreeNode root) {
-        dfs(root);
-        return ans ;
-    }
-    public void dfs(TreeNode node){
-        if (node == null){
-            return ;
-        }
-        table.put(node.val,table.getOrDefault(node.val,0) + 1);
-        if (node.left == null && node.right == null){
-            if (isValid(table))
-                ans ++ ;
-            table.put(node.val,table.get(node.val) - 1);
-            return ;
-        }
-        dfs(node.left);
-        dfs(node.right);
-        table.put(node.val,table.get(node.val) - 1);
-        return ;
-    }
-    public boolean isValid(HashMap<Integer,Integer> table){
-        int cnt = 0 ;
-        for (Map.Entry<Integer,Integer> entry : table.entrySet()){
-            if (entry.getValue() % 2 == 1)
-                cnt ++;
-        }
-        return cnt <= 1 ; 
-    }
-}
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
+        
+        ans = 0 ; path = collections.defaultdict(int)
+        def dfs(node):
+            nonlocal ans ; nonlocal path
+            if not node:
+                return
+            path[node.val] += 1
+            if not node.left and not node.right:
+                ans += 1 if isValid(path) else 0
+                path[node.val] -= 1
+                return
+            dfs(node.left)
+            dfs(node.right)
+            path[node.val] -= 1
+            return
+
+        def isValid(path):
+            cnt = 0
+            for i in path:
+                if path[i] % 2 != 0:
+                    cnt += 1
+            return cnt <= 1
+        dfs(root)
+        return ans
