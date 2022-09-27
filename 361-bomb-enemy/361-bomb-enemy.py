88a4f53bef80639ce814acac630f5fac2d092728
WenJun Wu <73014365+June24-Wu@users.noreply.github.com>
2022-09-26 22:51:42 -0500

    Time: 4105 ms (5.02%), Space: 60.3 MB (7.98%) - LeetHub

--- 361-bomb-enemy/361-bomb-enemy.py
+++ 361-bomb-enemy/361-bomb-enemy.py
@@ -1,35 +1,35 @@
 class Solution:
     def maxKilledEnemies(self, grid: List[List[str]]) -> int:
-        table = {}
         m = len(grid) ; n = len(grid[0])
-        def bfs(row,col):
-            return __bfs(row,col,"R") + __bfs(row,col,"L") + __bfs(row,col,"D") + __bfs(row,col,"U")
+        updown = [[[0,0] for _ in range(n)] for _ in range(m)]
+        bottomup = [[[0,0] for _ in range(n)] for _ in range(m)]
         
-        def __bfs(row,col,direction):
-            if (row,col,direction) in table:
-                return table[(row,col,direction)]
-            if row < 0 or row >= m or col < 0 or col >= n or grid[row][col] == "W":
-                return 0
-            if direction == "R":
-                newrow = row ; newcol = col + 1
-            elif direction == "L":
-                newrow = row ; newcol = col - 1
-            elif direction == "U":
-                newrow  = row - 1 ; newcol = col
-            else:
-                newrow = row + 1 ; newcol = col
-            if grid[row][col] == "E":
-                table[(row,col,direction)] = 1 + __bfs(newrow,newcol,direction)
-                return table[(row,col,direction)] 
-            else:
-                table[(row,col,direction)] = __bfs(newrow,newcol,direction)
-                return table[(row,col,direction)]
+        def get(dp,row,col,status):
+            if 0 <= row < m and 0<= col < n:
+                return dp[row][col][status]
+            return 0
+        def getGrid(row,col):
+            if 0 <= row < m and 0<= col < n:
+                return grid[row][col]
+            return None
         ans = 0
         for i in range(m):
             for j in range(n):
+                if grid[i][j] != "W":
+                    updown[i][j][0] += get(updown,i,j-1,0) + (getGrid(i,j-1) == "E")
+                    updown[i][j][1] += get(updown,i-1,j,1) + (getGrid(i-1,j) == "E")
+                    
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                if grid[i][j] != "W":
+                    bottomup[i][j][0] += get(bottomup,i,j+1,0) + (getGrid(i,j+1) == "E")
+                    bottomup[i][j][1] += get(bottomup,i+1,j,1) + (getGrid(i+1,j) == "E")
+
+        for i in range(m-1,-1,-1):
+            for j in range(n-1,-1,-1):
                 if grid[i][j] == "0":
-                    ans = max(ans,bfs(i,j))
+                    ans = max(ans,sum(bottomup[i][j]) + sum(updown[i][j]))
+        # print(dp)
         return ans
-                
                 
         
