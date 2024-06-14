e481c7e4e8b6a656ff000a5e397409bb4069bbc0
June Wu <73014365+June24-Wu@users.noreply.github.com>
2024-06-13 19:27:16 -0500

    Time: 629 ms (70.27%) | Memory: 30.9 MB (39.86%) - LeetSync

--- 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.py
+++ 982-minimum-increment-to-make-array-unique/minimum-increment-to-make-array-unique.py
@@ -2,7 +2,7 @@ class Solution:
     def minIncrementForUnique(self, nums: List[int]) -> int:
         nums.sort()
         left = 0 ; ans = 0
-        for i in range(3 * 10**5):
+        for i in range(2 * 10**5):
             if left == len(nums):
                 break
             if i < nums[left]:
