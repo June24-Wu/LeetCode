560c196aa6d07fa4f5dca5311246f5a3314e41e7
June Wu <73014365+June24-Wu@users.noreply.github.com>
2025-08-17 20:03:00 -0700

    Time: 519 ms (75.00%) | Memory: 31.4 MB (100.00%) - LeetSync

---
+++ 3966-minimum-sum-after-divisible-sum-deletions/minimum-sum-after-divisible-sum-deletions.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minArraySum(self, nums: List[int], k: int) -> int:
+        total = sum(nums)
+        dp = [-inf]*k
+        dp[0] = prefix = max_delete = 0
+
+        for val in nums:
+            prefix += val
+            r = prefix % k
+
+            candidate = dp[r] + prefix
+            max_delete = max(max_delete,candidate)
+            dp[r] = max(dp[r],max_delete-prefix)
+        
+        return total-max_delete
