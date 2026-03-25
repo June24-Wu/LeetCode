9d1cb76f777f90dbb0fe469262bfce8b16fd969e
June Wu <73014365+June24-Wu@users.noreply.github.com>
2026-03-25 10:27:43 -0700

    Time: 4 ms (35.89%) | Memory: 19.3 MB (64.00%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,30 +1,28 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        total = 0
-        rotten = collections.deque()
         m = len(grid); n = len(grid[0])
+
+        rot = []; total = 0; vis = set()
         for i in range(m):
             for j in range(n):
+                if grid[i][j] == 2:
+                    rot.append((i,j))
+                    vis.add((i,j))
                 if grid[i][j] != 0:
                     total += 1
-                if grid[i][j] == 2:
-                    rotten.append((i,j))
-                    total -= 1
         if total == 0:
             return 0
-        days = -1
-        while rotten:
-            length = len(rotten)
+        ans = 0
+        while rot:
+            length = len(rot)
+            # print(rot,ans)
             for _ in range(length):
-                r, c = rotten.popleft()
-                for i, j in [(0,1),(0,-1),(1,0),(-1,0)]:
-                    newr = r+i; newc=c+j
-                    # if 0 <= newr < m and 0 <= newc < n:
-                        # print(newr,newc,grid[newr][newc])
-                    if 0 <= newr < m and 0 <= newc < n and grid[newr][newc]==1:
-                        grid[newr][newc] = 2
-                        total -= 1
-                        rotten.append((newr,newc))
-            days += 1
-        return days if total == 0 else -1
-
+                r, c = rot.pop(0)
+                for i, j in [(1,0),(0,1),(-1,0),(0,-1)]:
+                    newr, newc = r+i, c+j
+                    if 0<= newr<m and 0<= newc < n and (newr,newc) not in vis and grid[newr][newc] == 1:
+                        vis.add((newr,newc))
+                        rot.append([newr,newc])
+            ans += 1
+        return ans - 1 if len(vis) == total else -1
+        
