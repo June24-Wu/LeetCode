89f378d1ecd3c7a5b349147df0547365ecf5dc96
June Wu <73014365+June24-Wu@users.noreply.github.com>
2026-07-20 11:37:11 -0700

    Time: 39 ms (17.02%) | Memory: 27.7 MB (14.11%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,20 +1,16 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        pre = [None for _ in range(n)]
-        su = [None for _ in range(n)]
-        curr = 1
-        for i in range(n):
-            curr *= nums[i]
-            pre[i] = curr
-        curr = 1
-        for i in range(n-1,-1,-1):
-            curr *= nums[i]
-            su[i] = curr
-        for i in range(n):
-            prev = pre[i-1] if i-1 >= 0 else 1
-            suv = su[i+1] if i+1 < n else 1
-            nums[i] = prev * suv
-        return nums
+        left = [None for _ in range(n)]
+        right = [None for _ in range(n)]
 
-
+        left[0] = 1
+        for i in range(1,n):
+            left[i] = nums[i-1] * left[i-1]
+        right[-1] = 1
+        for i in range(n-2,-1,-1):
+            right[i] = nums[i+1] * right[i+1]
+        # print(left)
+        # print(right)
+        return [left[i] * right[i] for i in range(n)]
+        
